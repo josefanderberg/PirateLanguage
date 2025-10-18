014e4cac7772f1807fcbbc71a74684fbcc8901f7
josefanderberg <josef.anderberg@gmail.com>
2025-10-18 22:46:01 +0200

    Implement BackFromPirate function and update PirateLang logic

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 var text = "Skriv en funktion till vilken man skickar ett antal ord - funktionen ska returnera det längsta ordet";
 const letter = "a"
-const constants = [ "a", "e", "i", "o", "u", "y", "å", "ä", "ö" ];
-
+const constants = [ "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", 
+    "n", "p", "q", "r", "s", "t", "v", "w", "x", "z" ];
 
 //const isConstChar = WordCheck(letter, constants);
  
-PirateLang(text, constants)
+pirateResult = PirateLang(text, constants)
+
+showOriginalText = BackFromPirate(pirateResult)
+console.log(showOriginalText)
+
+function BackFromPirate (captain) {
+
+    let originalText = []
+    let removeNextChar = false
+
+    for (var char of captain) {
+        if (char.toLowerCase() == "o") {
+            removeNextChar = true;
+            continue;
+        }
+        else if (removeNextChar == true){
+            removeNextChar = false;
+            continue
+        }
+        else {
+            originalText.push(char)
+        }
+    }
+    let returnText = originalText.join("")
+    
+    return returnText
+}
+
 /* const longWord = LongestWord(text)
 console.log(longWord) */
 
@@ -19,23 +46,23 @@ function WordCheck(letter, constants){
             console.log(constants[i])
             return true
         }
-        else{
-            return false
-        }
+
     }
+    return false
 }
 
 function LongestWord(text){
     const words = text.split(" ").sort()
     console.log(words)
 
-    var wordLetters = 0
+    let wordLetters = 0
+    let longestWord
     
 
     for (i = 0; i < words.length; i++){
         if (words[i].length > wordLetters){
             wordLetters = words[i].length;
-            var longestWord = words[i];
+            longestWord = words[i];
         }
     }
 
@@ -46,26 +73,20 @@ function LongestWord(text){
 function PirateLang(text, constants) {
 
     var pirateText = [];
-    var pirateDone = "";
-    var o = "o"
 
-    text.split("")
-    for (i = 0; i < text.length; i++){
+    for (const char of text) { // 'char' blir den aktuella bokstaven
+        let pirateAdd = char
 
         for (const constant of constants)
         {
 
-            if (constant == text[i]){
-                text[i].concat("o")
-            } 
-
+            if (constant == char.toLowerCase()){
+                pirateAdd = char.concat("o", char.toLowerCase())
+                break
+            }
         }
-    pirateText.push(text[i]);
+        pirateText.push(pirateAdd)
     }
-
-    for (var p of pirateText){
-        pirateDone = pirateDone.concat(p)
-    }
-
-    console.log(pirateDone)
+    return pirateResult = pirateText.join("");
 }
+
